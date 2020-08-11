f01f08d6ae37eb964a84e97f91aa29d19216f347
Peter Nguyen <nguyen.peter24@gmail.com>
2020-08-11 13:58:36 -0700

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -10,7 +10,7 @@ center: [40, -122],
 // Adding a tile layer (the background map image) to our map
 // We use the addTo method to add objects to our map
 L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  attribution: "© <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
   tileSize: 512,
   maxZoom: 18,
   id: "mapbox/streets-v11",
