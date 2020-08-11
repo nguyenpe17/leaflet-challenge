73ad1602287005c0f2e24dbae4fbf087384f3c05
Peter Nguyen <nguyen.peter24@gmail.com>
2020-08-11 13:44:41 -0700

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -20,86 +20,115 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 // Store our API endpoint
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
 
-//  GET color radius call to the query URL
-d3.json(queryUrl, function(data) {
-  function styleInfo(feature) {
-    return {
-      opacity: 1,
-      fillOpacity: 1,
-      fillColor: getColor(feature.properties.mag),
-      color: "#000000",
-      radius: getRadius(feature.properties.mag),
-      stroke: true,
-      weight: 0.5
-    };
-  }
-  // set different color from magnitude
-    function getColor(magnitude) {
-    switch (true) {
-    case magnitude > 5:
-      return "#ea2c2c";
-    case magnitude > 4:
-      return "#ea822c";
-    case magnitude > 3:
-      return "#ee9c00";
-    case magnitude > 2:
-      return "#eecc00";
-    case magnitude > 1:
-      return "#d4ee00";
-    default:
-      return "#98ee00";
-    }
-  }
-  // set radiuss from magnitude
-    function getRadius(magnitude) {
-    if (magnitude === 0) {
-      return 1;
-    }
+function markerSize(mag) {
+  return mag * 30000;
+}
 
-    return magnitude * 4;
+function markerColor(mag) {
+  if (mag <= 1) {
+      return "#ADFF2F";
+  } else if (mag <= 2) {
+      return "#9ACD32";
+  } else if (mag <= 3) {
+      return "#FFFF00";
+  } else if (mag <= 4) {
+      return "#ffd700";
+  } else if (mag <= 5) {
+      return "#FFA500";
+  } else {
+      return "#FF0000";
+  };
+}
+
+// Perform a GET request to the query URL
+d3.json(link, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
+
+function createFeatures(earthquakeData) {
+
+  var earthquakes = L.geoJSON(earthquakeData, {
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+ onEachFeature : function (feature, layer) {
+
+    layer.bindPopup("<h3>" + feature.properties.place +
+      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>" + "<p> Magnitude: " +  feature.properties.mag + "</p>")
+    },     pointToLayer: function (feature, latlng) {
+      return new L.circle(latlng,
+        {radius: markerSize(feature.properties.mag),
+        fillColor: markerColor(feature.properties.mag),
+        fillOpacity: 1,
+        stroke: false,
+    })
   }
-    // GeoJSON layer
-    L.geoJson(data, {
-      // Maken cricles
-      pointToLayer: function(feature, latlng) {
-        return L.circleMarker(latlng);
-      },
-      // cirecle style
-      style: styleInfo,
-      // popup for each marker
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-      }
-    }).addTo(myMap);
-  
-    // an object legend
-    var legend = L.control({
-      position: "bottomright"
-    });
-  
-    // details for the legend
-    legend.onAdd = function() {
-      var div = L.DomUtil.create("div", "info legend");
-  
-      var grades = [0, 1, 2, 3, 4, 5];
-      var colors = [
-        "#98ee00",
-        "#d4ee00",
-        "#eecc00",
-        "#ee9c00",
-        "#ea822c",
-        "#ea2c2c"
-      ];
-  
-      // Looping through
-      for (var i = 0; i < grades.length; i++) {
-        div.innerHTML +=
-          "<i style='background: " + colors[i] + "'></i> " +
-          grades[i] + (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
-      }
-      return div;
-    };
-  
-    // Finally, we our legend to the map.
-    legend.addTo(myMap);
   });
+    
+
+
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+}
+
+function createMap(earthquakes) {
+
+  // Define satelitemap and darkmap layers
+  var satelitemap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+  });
+
+  var darkmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: API_KEY
+  });
+
+  // Define a baseMaps object to hold our base layers
+  var baseMaps = {
+    "Satelite Map": satelitemap,
+    "Dark Map": darkmap
+  };
+
+  // Create overlay object to hold our overlay layer
+  var overlayMaps = {
+    Earthquakes: earthquakes
+  };
+
+  // Create our map, giving it the satelitemap and earthquakes layers to display on load
+  var myMap = L.map("map", {
+    center: [31.57853542647338,-99.580078125],
+    zoom: 3,
+    layers: [satelitemap, earthquakes]
+  });
+
+  // Create a layer control
+  // Pass in our baseMaps and overlayMaps
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+  var legend = L.control({position: 'bottomright'});
+
+  legend.onAdd = function () {
+  
+      var div = L.DomUtil.create('div', 'info legend'),
+          magnitudes = [0, 1, 2, 3, 4, 5];
+  
+      for (var i = 0; i < magnitudes.length; i++) {
+          div.innerHTML +=
+              '<i style="background:' + markerColor(magnitudes[i] + 1) + '"></i> ' + 
+      + magnitudes[i] + (magnitudes[i + 1] ? ' - ' + magnitudes[i + 1] + '<br>' : ' + ');
+      }
+  
+      return div;
+  };
+  
+  legend.addTo(myMap);
+
+}
